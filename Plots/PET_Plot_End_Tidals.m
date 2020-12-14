2f8a40fa31c49d702b4d09a3deac59136c1eabc2
tombullock <twbullock@googlemail.com>
2020-12-14 10:49:24 -0800

    PET

--- Plots/PET_Plot_End_Tidals.m
+++ Plots/PET_Plot_End_Tidals.m
@@ -30,12 +30,15 @@ for iPlot=1:4
     yyaxis left
     shadedErrorBar(1:90,thisMean(:,2),thisSEM(:,2),{'color','b','linestyle','-'}); hold on
     set(gca,'ylim',[40,100],'LineWidth',1.5,'YColor','b','box','off','fontsize',18,'xlim',[1,90],'xtick',[1:10:90,90],'XTickLabel',[0,10:10:90]);
+   
     
     % plot CO2
     yyaxis right
     shadedErrorBar(1:90,thisMean(:,1),thisSEM(:,1),{'color','r','linestyle','-'});
     set(gca,'ylim',[20,60],'ytick',[20:10:60],'LineWidth',1.5,'YColor','r','box','off','fontsize',18,'xlim',[1,90],'XTick',[1:10:90,90],'XTickLabel',[0,10:10:90]);
     
+    line([45,45],[20,60],'linestyle','--','color','k','linewidth',2);
+
 end
 
 saveas(h,[destDir '/' 'PET_Traces.jpg'],'jpeg')
@@ -58,6 +61,8 @@ for iPlot=1
     yyaxis right
     shadedErrorBar(1:90,thisMean(:,1),thisSEM(:,1),{'color','r','linestyle','-'});
     set(gca,'ylim',[20,60],'ytick',[20:10:60],'LineWidth',1.5,'YColor','r','box','off','fontsize',18,'xlim',[1,90],'XTick',[1:10:90,90],'XTickLabel',[0,10:10:90]);
+    
+    line([45,45],[20,60],'linestyle','--','color','k','linewidth',2);
     
 end
 
